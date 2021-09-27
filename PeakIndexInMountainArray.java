9fcff126ff1fdf5ffe8307bfb1e47dc92f45c730
Shubham-Singh-Rajput <88304238+shubham-singh-748@users.noreply.github.com>
2021-09-27 14:43:28 +0530

    Create PeakIndexInMountainArray.java

---
+++ PeakIndexInMountainArray.java
@@ -0,0 +1,22 @@
+//PROBLEM LINK : https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start) /2;
+            if(arr[mid] > arr[mid + 1])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
